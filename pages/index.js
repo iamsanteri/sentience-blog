46e864f54b399a7994c619b8cdfa3e50508033e0
Santeri Liukkonen <santeri@Santeris-MacBook-Air.local>
2023-03-17 09:49:12 +0200

    Added tailwind and modified splash screen

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,12 @@ export default function Home() {
       <Head>
         <title>Sentience Platform</title>
       </Head>
-      <div>
-        <h1>Hello Sentience platform!</h1>
-      </div>
+      <section class="bg-white dark:bg-gray-900">
+        <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+          <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Welcome to Sentience platform!</h1>
+          <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">We'll keep building it here.</p>
+        </div>
+      </section>
     </>
   )
 }
