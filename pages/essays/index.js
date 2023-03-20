bbdc6373e0141e37b9bae891e38e42999d6e27fa
Santeri Liukkonen <santeri@Santeris-MacBook-Air.local>
2023-03-20 13:48:33 +0200

    Adjusted vertical padding

--- pages/essays/index.js
+++ pages/essays/index.js
@@ -15,7 +15,7 @@ export default function Essays() {
         <title>Essays - Sentience</title>
       </Head>
       <section className="bg-white dark:bg-sentient pt-20 lg:pt-24">
-        <div className="py-8 lg:py-10 px-4 mx-auto max-w-screen-xl text-center lg:px-12">
+        <div className="py-8 lg:py-14 px-4 mx-auto max-w-screen-xl text-center lg:px-12">
           <h1 className="mb-1 text-2xl font-semibold leading-none tracking-tight text-gray-900 md:text-2xl lg:text-3xl dark:text-white">Essays</h1>
         </div>
       </section>
