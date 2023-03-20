ff025ecda82d33e2f7613b4376e0643c4e3cc847
Santeri Liukkonen <santeri@Santeris-MacBook-Air.local>
2023-03-20 17:26:16 +0200

    Modified line height

--- pages/essays/[id].js
+++ pages/essays/[id].js
@@ -33,7 +33,7 @@ export default function SinglePost({ postData }) {
             <div className="w-full flex justify-center">
                 <div className="p-5 md:w-3/4 lg:w-3/5 xl:w-2/4 2xl:w-2/5">
                     <h1 className="mb-9 text-2xl md:text-4xl font-semibold leading-none tracking-tight text-gray-900 dark:text-white">{ postData.title }</h1>
-                    <article className="prose prose-lg lg:prose-xl text-gray-700 dark:text-gray-300" dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+                    <article style={{ lineHeight: 1.9 + "rem"}} className="prose prose-lg lg:prose-xl text-gray-700 dark:text-gray-300" dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
                 </div>
             </div>
         </section>
