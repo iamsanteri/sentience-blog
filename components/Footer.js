3685fcab16606e2a21000bb056743aecb28ec231
Santeri Liukkonen <santeri@Santeris-MacBook-Air.local>
2023-03-21 01:38:32 +0200

    Adjusted footer menu items spacing on mobile

--- components/Footer.js
+++ components/Footer.js
@@ -8,15 +8,15 @@ const Footer = () => {
         <p className="inline-block align-center max-lg:py-8">Sentience © { new Date().getFullYear() }</p>
         <div className="justify-center lg:flex">
           <span className="flex-center max-lg:flex max-lg:flex-col">
-            <span className="whitespace-nowrap max-lg:py-0.5 p-3 hover:text-black dark:hover:text-white">
+            <span className="whitespace-nowrap max-lg:py-2 p-3 hover:text-black dark:hover:text-white">
               <Link href="/stories">Stories</Link>
             </span>
             <span className="text-xs max-lg:hidden">•</span>
-            <span className="whitespace-nowrap max-lg:py-0.5 p-3 hover:text-black dark:hover:text-white">
+            <span className="whitespace-nowrap max-lg:py-2 p-3 hover:text-black dark:hover:text-white">
               <Link href="/essays">Essays</Link>
             </span>
             <span className="text-xs max-lg:hidden">•</span>
-            <span className="whitespace-nowrap max-lg:py-0.5 p-3 hover:text-black dark:hover:text-white">
+            <span className="whitespace-nowrap max-lg:py-2 p-3 hover:text-black dark:hover:text-white">
               <Link href="/about">About</Link>
             </span>
           </span>
